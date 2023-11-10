48045d52985a9d4a0a26072598059222d2a49d24
YOS0602 <yoshitaka.t.biz@gmail.com>
2023-11-11 00:54:10 +0900

    2022-2 part2

--- src/2022/2/index.test.ts
+++ src/2022/2/index.test.ts
@@ -1,5 +1,10 @@
 import { test, expect } from 'vitest';
-import { calculateTotalScore, getScoreOfEachRound } from '.';
+import {
+  calculateTotalScore,
+  calculateTotalScoreBasedOnGuide,
+  getScoreOfEachRound,
+  getScoreOfEachRoundBasedOnGuide,
+} from '.';
 
 test('戦略に従ってじゃんけんの結果を算出できること', () => {
   // given
@@ -19,4 +24,24 @@ test('1ラウンドのじゃんけん勝負のスコア計算ができること'
   const actual = getScoreOfEachRound(input);
   // then
   expect(actual).toBe(8);
+});
+
+test('ultra top secret strategy guideに従ってじゃんけんをした結果を算出できること', () => {
+  // given
+  const input = `A Y
+B X
+C Z`;
+  // when
+  const actual = calculateTotalScoreBasedOnGuide(input);
+  // then
+  expect(actual).toBe(12);
+});
+
+test('ultra top secret strategy guideに従って、1ラウンドあたりのスコアを計算できること', () => {
+  // given
+  const input = 'A Y';
+  // when
+  const actual = getScoreOfEachRoundBasedOnGuide(input);
+  // then
+  expect(actual).toBe(4);
 });
--- src/2022/2/index.ts
+++ src/2022/2/index.ts
@@ -1,12 +1,18 @@
 type OpponentShapes = 'A' | 'B' | 'C';
 type MyShapes = 'X' | 'Y' | 'Z';
+type Result = 'draw' | 'win' | 'lose';
 type DataByShape = {
   A: number;
   B: number;
   C: number;
   score: number;
+  choice: Result;
 };
 type Mappings = Record<MyShapes, DataByShape>;
+
+// A Rock X
+// B Paper Y
+// C Scissors Z
 
 const mappings: Mappings = {
   X: {
@@ -14,26 +20,44 @@ const mappings: Mappings = {
     B: 0, // 'lose',
     C: 6, // 'win',
     score: 1,
+    choice: 'lose',
   },
   Y: {
     A: 6, // 'win',
     B: 3, // 'draw',
     C: 0, // 'lose',
     score: 2,
+    choice: 'draw',
   },
   Z: {
     A: 0, // 'lose',
     B: 6, // 'win',
     C: 3, // 'draw',
     score: 3,
+    choice: 'win',
+  },
+};
+
+const mappings2 = {
+  A: {
+    win: mappings.Y.score + 6, // Aに対して勝てる選択肢
+    draw: mappings.X.score + 3,
+    lose: mappings.Z.score, // Aに対して負ける選択肢
+  },
+  B: {
+    win: mappings.Z.score + 6,
+    draw: mappings.Y.score + 3,
+    lose: mappings.X.score,
+  },
+  C: {
+    win: mappings.X.score + 6,
+    draw: mappings.Z.score + 3,
+    lose: mappings.Y.score,
   },
 };
 
 export function calculateTotalScore(input: string) {
-  return input
-    .split('\n')
-    .map((round) => getScoreOfEachRound(round))
-    .reduce((p, c) => p + c, 0);
+  return calculateService(input, getScoreOfEachRound);
 }
 
 export function getScoreOfEachRound(input: string) {
@@ -41,4 +65,25 @@ export function getScoreOfEachRound(input: string) {
   const { score, ...rest } = mappings[myShape as MyShapes];
   const result = rest[opponentShape as OpponentShapes];
   return score + result;
+}
+
+// Part2
+export function calculateTotalScoreBasedOnGuide(input: string) {
+  return calculateService(input, getScoreOfEachRoundBasedOnGuide);
+}
+
+export function getScoreOfEachRoundBasedOnGuide(input: string) {
+  const [opponentShape, myShape] = input.split(' ');
+  const { choice } = mappings[myShape as MyShapes];
+  return mappings2[opponentShape as OpponentShapes][choice];
+}
+
+function calculateService(
+  input: string,
+  func: (input: string) => number
+): number {
+  return input
+    .split('\n')
+    .map((round) => func(round))
+    .reduce((p, c) => p + c, 0);
 }
